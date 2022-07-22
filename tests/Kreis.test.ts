6812f1798647357b9a8a73526a9b4c76a03adbb2
erichdb <erichsdb@gmail.com>
2022-07-22 18:11:31 +0200

    + added complex example

--- tests/Kreis.test.ts
+++ tests/Kreis.test.ts
@@ -4,12 +4,12 @@ test("Kreis besitzt einen Kreis", () => {
   var g = new Graph();
   var vertices = ["A", "B", "C", "D"];
 
-  // adding vertices
+  // Knoten hinzufügen
   for (var i = 0; i < vertices.length; i++) {
     g.addVertex(vertices[i]);
   }
 
-  // adding edges
+  // Kanten hinzufügen
   g.addEdge("A", "B");
   g.addEdge("B", "C");
   g.addEdge("C", "D");
@@ -36,12 +36,12 @@ test("K_4 besitzt einen Kreis", () => {
   var g = new Graph();
   var vertices = ["A", "B", "C", "D"];
 
-  // adding vertices
+  // Knoten hinzufügen
   for (var i = 0; i < vertices.length; i++) {
     g.addVertex(vertices[i]);
   }
 
-  // adding edges
+  // Kanten hinzufügen
   g.addEdge("A", "B");
   g.addEdge("A", "C");
   g.addEdge("A", "D");
@@ -64,4 +64,46 @@ test("K_4 besitzt einen Kreis", () => {
 
   // Komponenten vergleichen
   expect(g.circle).toEqual(["A", "B", "C", "A"]);
+});
+
+test("Komplexes Beispiel besitzt einen Kreis", () => {
+  var g = new Graph();
+  var vertices = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'U', 'V'];
+  
+  // Knoten hinzufügen
+  for (var i = 0; i < vertices.length; i++) {
+      g.addVertex(vertices[i]);
+  }
+   
+  // Kanten hinzufügen
+  g.addEdge('U', 'A');
+  g.addEdge('U', 'J');
+  g.addEdge('A', 'B');
+  g.addEdge('B', 'C');
+  g.addEdge('I', 'J');
+  g.addEdge('C', 'D');
+  g.addEdge('E', 'I');
+  g.addEdge('E', 'A');
+  g.addEdge('D', 'F');
+  g.addEdge('E', 'G');
+  g.addEdge('E', 'V');
+  g.addEdge('V', 'F');
+  g.addEdge('V', 'H');
+  g.addEdge('F', 'H');
+  g.addEdge('F', 'G');
+
+  // Start- und Endwerte definieren
+  var start = 'U';
+  var end = 'V'
+  
+  // Kürzesten Weg von Start zu Ziel finden mit Breitensuche
+  g.bfs(start);
+  g.modify_adjacency_list(g.find_path(start, end));
+  
+  // Tiefensuche mit Low-Werten
+  g.dfs(start);
+  
+  g.create_circle(start, end);
+
+  expect(g.circle).toEqual(['U', 'J', 'I', 'E', 'V', 'F', 'D', 'C', 'B', 'A', 'U']);
 });
