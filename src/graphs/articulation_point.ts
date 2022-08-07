5cb70ef6efe3fea012df0a31325108b725dbbf77
erichdb <erichsdb@gmail.com>
2022-08-08 00:58:04 +0200

    minimal working example

--- src/graphs/articulation_point.ts
+++ src/graphs/articulation_point.ts
@@ -2,7 +2,7 @@ import { Graph } from "../Graph";
 
 export function create_articulation_point_graph() {
   var g = new Graph();
-  var vertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "U", "V"];
+  var vertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "U", "V"];
 
   // Knoten hinzufügen
   for (var i = 0; i < vertices.length; i++) {
@@ -10,26 +10,17 @@ export function create_articulation_point_graph() {
   }
 
   // Kanten hinzufügen
-g.addEdge("U", "A");  
-g.addEdge("A", "B");
-g.addEdge("B", "C");
-g.addEdge("C", "D");
-g.addEdge("D", "E");
-g.addEdge("E", "F");
-g.addEdge("F", "V");
-g.addEdge("V", "G");
-g.addEdge("G", "H");
-g.addEdge("H", "I");
-g.addEdge("I", "F");
-g.addEdge("F", "J");
-g.addEdge("J", "E");
+  g.addEdge("U", "A");
+  g.addEdge("A", "B");
+  g.addEdge("B", "C");
+  g.addEdge("C", "D");
+  g.addEdge("D", "E");
+  g.addEdge("E", "F");
+  g.addEdge("F", "V");
+  g.addEdge("V", "G");
+  g.addEdge("G", "H");
+  g.addEdge("H", "I");
+  g.addEdge("I", "F");
 
-
-
-
-
-
-
-
-  return g
+  return g;
 }
